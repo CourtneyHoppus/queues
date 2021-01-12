e9502d5c32733b85bf63fa162f34a3eba2abc305
CourtneyHoppus <74206103+CourtneyHoppus@users.noreply.github.com>
2021-01-12 15:26:21 -0800

    Create RandomizedQueue.java

---
+++ RandomizedQueue.java
@@ -0,0 +1,52 @@
+/* *****************************************************************************
+ *  Name: Courtney Hoppus
+ *  Date: 2021-01-11
+ *  Description: Assignment 1
+ **************************************************************************** */
+
+import java.util.Iterator;
+
+public class RandomizedQueue<Item> implements Iterable<Item> {
+    private int size;
+
+    // construct an empty randomized queue
+    public RandomizedQueue() {
+        size = 0;
+    }
+
+    // is the randomized queue empty?
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    // return the number of items on the randomized queue
+    public int size() {
+        return size;
+    }
+
+    // add the item
+    public void enqueue(Item item) {
+
+    }
+
+    // remove and return a random item
+    public Item dequeue() {
+
+    }
+
+    // return a random item (but do not remove it)
+    public Item sample() {
+
+    }
+
+    // return an independent iterator over items in random order
+    public Iterator<Item> iterator() {
+
+    }
+
+    // unit testing (required)
+    public static void main(String[] args) {
+
+    }
+
+}
