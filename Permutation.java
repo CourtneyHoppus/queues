5f8e9a691a29f214a9c83b3d4a5229a0afdeaddf
CourtneyHoppus <74206103+CourtneyHoppus@users.noreply.github.com>
2021-01-28 13:08:47 -0800

    Started Permutation

--- Permutation.java
+++ Permutation.java
@@ -4,8 +4,18 @@
  *  Description:
  **************************************************************************** */
 
+import edu.princeton.cs.algs4.StdIn;
+import edu.princeton.cs.algs4.StdOut;
+
 public class Permutation {
     public static void main(String[] args) {
-
+        RandomizedQueue<String> randoQ = new RandomizedQueue<String>();
+        int num = Integer.parseInt(args[0]);
+        while (!StdIn.isEmpty()) {
+            randoQ.enqueue(StdIn.readString());
+        }
+        for (int idx = 0; idx < num; idx++) {
+            StdOut.println(randoQ.dequeue());
+        }
     }
 }
